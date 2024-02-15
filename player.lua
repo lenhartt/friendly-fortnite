f30561890b7d6959616055ddc1b714128567db01
lenhartt <129159873+lenhartt@users.noreply.github.com>
2024-02-15 22:21:05 +0100

    updatet player.lua

--- player.lua
+++ player.lua
@@ -2,13 +2,16 @@ local player = {}
 
 function player.physics()
     local grav = 5
-    
+    local vel = 3
+
 end
 
 
 function player.draw()
-    --player.x, player.y = love.graphics.getWidth()/2, love.graphics.getHeight()/2
-    love.graphics.circle("fill", 0, -10, 5)
+    player.x, player.y = 10, -10
+    player.radius = 5
+    
+    love.graphics.circle("fill", player.x, player.y, player.radius)
 end
 
 return player
